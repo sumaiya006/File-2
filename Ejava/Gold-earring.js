65814caf0abeeac97f41ab2d311649ab7448c517
sumaiya006 <sumaiyab071@gmail.com>
2021-09-16 22:26:32 +0600

    Artistry

---
+++ Ejava/Gold-earring.js
@@ -0,0 +1,28 @@
+var sld=document.getElementById("slider");
+
+var i=1;
+
+setInterval(auto_slideshow, 2000);
+
+function auto_slideshow(){
+	
+	console.log(i);
+	
+	if(i==1){
+		sld.setAttribute("style",  "transform: translate(-33.33%, 0%)");
+	}
+	
+	else if(i==2){
+		sld.setAttribute("style", "transform: translate(-66.66%, 0%)");
+	}
+	
+	else if(i==3){
+		sld.setAttribute("style", "transform: translate(0%, 0%)");
+	}
+	
+	i++;
+	if(i>3){
+		i=1;
+	}
+	
+}
